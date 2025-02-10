f5b9799a4eee6da8b427dfde23862859375058e3
MuhiuddinHomeDesktop <muhiuddinanik98@gmail.com>
2025-02-10 08:29:43 +0600

    add datetime views in url

--- firstapp/views.py
+++ firstapp/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+import datetime
 def display(request):
     return HttpResponse('<h1> Hello World </h1>')
 
-# Create your views here.
+def date_time(request):
+    date=datetime.datetime.now()
+    s='<h1> The current date and time at server is: '+str(date)+'</h1>'
+    return HttpResponse(s)
--- firstproject/urls.py
+++ firstproject/urls.py
@@ -21,4 +21,5 @@ from firstapp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/',views.display),
+    path('date/',views.date_time),
 ]
